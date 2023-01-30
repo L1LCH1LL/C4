8bf1869bb0e8dc6af6f7548c30d86e06dde5d55a
L1lCH1LL <mr.kudinov-2004@mail.ru>
2023-01-31 00:30:00 +0300

    25

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число которое возводится степень");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int num2 = Convert.ToInt32(Console.ReadLine());
+double num3 = Math.Pow(num1,num2);
+
+Console.WriteLine(num3);
